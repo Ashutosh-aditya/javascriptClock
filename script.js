d37b22530ee7f9b1138a2b6c718156cc3938bf47
Ashutosh aditya <adityaashutosh6@gmail.com>
2024-03-17 12:56:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const clockBody = document.getElementById('clockBody');
+
+function updateClock(){
+    const date = new Date;
+    var hour=date.getHours();
+    var minutes=date.getMinutes();
+    var seconds=date.getSeconds();
+    var suffix = hour >= 12 ? "PM" : "AM";
+
+    hour=hour>12? hour-12: hour;
+    hour=hour==0?0 : hour;
+
+    
+    hour = hour<10? "0"+hour :hour;
+    minutes=minutes<10? "0"+minutes :minutes;
+    seconds=seconds<10? "0"+seconds :seconds;
+
+    clockBody.innerHTML=hour+":"+minutes+":"+seconds+" "+suffix;
+    
+}
+
+setInterval(updateClock,1000);
